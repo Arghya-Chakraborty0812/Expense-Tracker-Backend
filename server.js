9530df854a7eb4a068d55566661889c3c2df5d11
Arghya Chakraborty <arghya0812@gmail.com>
2025-05-19 23:06:42 +0530

    bcryptjs

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
 const app = express();
